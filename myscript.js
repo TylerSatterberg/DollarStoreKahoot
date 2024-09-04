55ddb7099b03ef87d593c6fbc436674e8471e679
TylerSatterberg <satterbergtyler@gmail.com>
2024-09-04 13:55:56 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,36 +1,4 @@
  $(document). ready(function() {
-const questions = ["17?", "Is THAT Ryan Yang?","What is the Atomic number of technitium?", "What is the name of Jacks dog?", "Should Tyler read 'A Pickle for the Knowing Ones?" ]
-const answers = ["yes", "no", "maybe", "is that ryan yang", "yes", "no", "definitly", "IS THAT EDWARDOO PERKOO", "39", "43", "32", "11", "Miso", "Pepper", "Salt", "λοπαδο­τεμαχο­σελαχο­γαλεο­κρανιο­λειψανο­δριμ­υπο­τριμματο­σιλφιο­καραβο­μελιτο­κατακεχυ­μενο­κιχλ­επι­κοσσυφο­φαττο­περιστερ­αλεκτρυον­οπτο­κεφαλλιο­κιγκλο­πελειο­λαγῳο­σιραιο­βαφη­τραγανο­πτερύγων", "What is that" , "yes" , "Miso Soup", "Jack Fulbeck"]
-const correct = ["4", "1", "2", "4"]
- 
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   $("button").click(function(){
-     $("index.html").hide
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
+$("correct1").click(function(){
+ $("a").click();
  })
