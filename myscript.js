2a4fc3f671d09531f6bbeb06fa3377e9ec832e73
TylerSatterberg <satterbergtyler@gmail.com>
2024-10-15 12:21:16 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,11 @@
  $(document). ready(function() {
-$("correct1").click(function(){
- $("a").click();
+if ($('.current-question').hasClass('C')) {
+  $('.current-question').removeClass('current-question');
+  $('.current-question').next().addClass('current-question');
+}
+else:
+ ($('.current-question').hasClass('I')) {
+$('.current-question').removeClass('current-question');
+
+}
  })
