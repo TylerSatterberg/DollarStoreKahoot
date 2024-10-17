addb2ba366d913d60b5417c914c18d7fa77ff109
TylerSatterberg <satterbergtyler@gmail.com>
2024-10-17 12:54:40 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,11 +1,9 @@
- $(document). ready(function() {
-if ($('.current-question').hasClass('C')) {
-  $('.current-question').removeClass('current-question');
-  $('.current-question').next().addClass('current-question');
-}
-else:
- ($('.current-question').hasClass('I')) {
-$('.current-question').removeClass('current-question');
-$('.current-question').next().addClass('you-lose');
-}
- })
+$(document).ready(function() {
+  if ($('.current-question').hasClass('C')) {
+    $('.current-question').removeClass('current-question');
+    $('.current-question').next().addClass('current-question');
+  }
+  else if ($('.current-question').hasClass('I')) {
+    window.location.href = 'lose.html';
+  }
+});
